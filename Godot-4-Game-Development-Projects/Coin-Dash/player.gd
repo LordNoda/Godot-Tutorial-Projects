df33f949f093c7519138f359e598135199ca64dd
Jordan Nguyen <34908002+LordNoda@users.noreply.github.com>
2025-03-03 21:09:35 +1030

    Player animation switching and set up for start up and die funcs
    Switch the player animation based off input ,reading velocity
    Flip sprite h depending on velocity.
    create start and end functions to facilitate set up and game end on collision (next section)

--- Godot-4-Game-Development-Projects/Coin-Dash/player.gd
+++ Godot-4-Game-Development-Projects/Coin-Dash/player.gd
@@ -6,6 +6,8 @@ var screensize = Vector2(480, 720);
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# call start function to set up player on first call
+	start()
 	pass # Replace with function body.
 
 
@@ -18,4 +20,25 @@ func _process(delta):
 	# clamp movement to screen to keep within bounds
 	position = Vector2(clamp(position.x, 0, screensize.x), clamp(position.y, 0, screensize.y));
 	
+	# animation switcher determined by wether velocity has direction
+	if velocity.length() > 0:
+		$AnimatedSprite2D.animation = "run";
+	else:
+		$AnimatedSprite2D.animation = "idle";
+		
+	# flip animation based on velocity direction (if less than 0, going left flip image)
+	if velocity.x != 0:
+		$AnimatedSprite2D.flip_h = velocity.x <0;
+		
 	pass
+	
+# Initial set up
+func start():
+	set_process(true); # This tells godot to run process function for this node
+	position = screensize / 2;
+	$AnimatedSprite2D.animation = "idle";
+
+# On death function
+func die():
+	$AnimatedSprite2D.animation = "hurt";
+	set_process(false); # This tells godot to run stop running process function for this node
