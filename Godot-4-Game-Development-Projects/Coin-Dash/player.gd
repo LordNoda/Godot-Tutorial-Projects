13a3c6dfb38a457a64dd6eae43745cbbf3af3373
Jordan Nguyen <34908002+LordNoda@users.noreply.github.com>
2025-03-03 21:21:30 +1030

    Finished the Player.tscn
    Completed part 1 of the tutorial the entire player set up.
    Conclusion of this was adding the on area enter functions, checking what we entered then running a logic route emitting signals to listen to so other objects can respond as well as running player script logic (such as die)

--- Godot-4-Game-Development-Projects/Coin-Dash/player.gd
+++ Godot-4-Game-Development-Projects/Coin-Dash/player.gd
@@ -1,6 +1,13 @@
 extends Area2D
 
+# Signals
+signal pickup;
+signal hurt;
+
+# Export values
 @export var speed = 350;
+
+# Internal values
 var velocity = Vector2.ZERO;
 var screensize = Vector2(480, 720);
 
@@ -8,7 +15,6 @@ var screensize = Vector2(480, 720);
 func _ready():
 	# call start function to set up player on first call
 	start()
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,8 +35,6 @@ func _process(delta):
 	# flip animation based on velocity direction (if less than 0, going left flip image)
 	if velocity.x != 0:
 		$AnimatedSprite2D.flip_h = velocity.x <0;
-		
-	pass
 	
 # Initial set up
 func start():
@@ -42,3 +46,15 @@ func start():
 func die():
 	$AnimatedSprite2D.animation = "hurt";
 	set_process(false); # This tells godot to run stop running process function for this node
+
+
+func _on_area_entered(area):
+	# When hitting something we follow a logic route (2 separate if's here mean both can occur)
+	
+	if area.is_in_group("coins"):
+		area.pickup(); # Execute area.pickup method
+		pickup.emit(); # Emit the pickup signal
+		
+	if area.is_in_group("obstacles"):
+		hurt.emit(); # emit the hurt signal
+		die() # Call die signal (Hitting an obstacle = losing)
