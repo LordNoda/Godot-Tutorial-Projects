7ee5bec34302550c7ffa72a6ce6271d68afff288
Jordan Nguyen <34908002+LordNoda@users.noreply.github.com>
2025-03-20 21:05:09 +1030

    Starting to implement the bullet for Space Rocks

---
+++ Godot-4-Game-Development-Projects/Space-Rocks/bullet/bullet.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+@export var speed = 1000;
+
+var velocity = Vector2.ZERO;
+
+func start(_transform) :
+	transform = _transform;
+	velocity = transform.x * speed;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position += velocity * delta;
+
+# VisibleOnScreenNotifier will remove the bullet when it leaves the screen.
+# Actually very handy inbuild notifier/function
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free();
+
+# Destroy rock when hit, then remove self
+func _on_bullet_body_entered(body):
+	if body.is_in_group("rocks"):
+		body.explode();
+		queue_free();
